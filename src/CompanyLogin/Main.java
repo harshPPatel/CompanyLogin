4eb83611b075aae94e445e53d17db85e1a68fe91
harshPPatel <36562266+harshPPatel@users.noreply.github.com>
2018-11-06 19:12:53 -0600

    Initial commit

---
+++ src/CompanyLogin/Main.java
@@ -0,0 +1,37 @@
+package CompanyLogin;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        boolean isExit = false;
+        Scanner scanner = new Scanner(System.in);
+        int userStartChoice;
+
+        System.out.println("Choose Your Code : \n" +
+                           "  1 - Log In\n" +
+                           "  2 - Sign Up");
+        int i = 0;
+        while (i == 0) {
+            System.out.print("Enter your code : ");
+            userStartChoice = scanner.nextInt();
+
+            switch (userStartChoice) {
+                case 1:
+                    System.out.println("Log In");
+                    i++;
+                    break;
+                case 2:
+                    System.out.println("Sign Up");
+                    i++;
+                    break;
+                default:
+                    System.out.println("\nEnter Valid Input!!");
+                    i = 0;
+            }
+        }
+
+    }
+}
